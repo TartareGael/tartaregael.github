0ae44c6f9575f2790a2b3e157cdf833db7fe4dfe
TartareGael <129100946+TartareGael@users.noreply.github.com>
2026-01-09 23:20:02 +0100

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,3 +1,18 @@
-export async function onRequest() {
-  return new Response("MIDDLEWARE OK");
+export async function onRequest(context) {
+  const { request, next } = context;
+  const url = new URL(request.url);
+
+  // 🔐 Page protégée
+  if (url.pathname === "/constel-mentorat") {
+    const cookie = request.headers.get("Cookie") || "";
+
+    if (!cookie.includes("constel_access=true")) {
+      return Response.redirect(
+        `${url.origin}/login-constel-mentorat`,
+        302
+      );
+    }
+  }
+
+  return next();
 }
