d5c28f47de0e37e96f2f0b943688dc9566f79536
Gaël Tartare <g.tartare@yahoo.fr>
2026-03-24 23:25:53 +0100

    update

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,21 +1,3 @@
 export async function onRequest(context) {
-  const { request, next } = context;
-
-  const url = new URL(request.url);
-
-  // On protège uniquement la page constel-mentorat
-  if (url.pathname.startsWith("/constel-mentorat")) {
-    const cookie = request.headers.get("cookie") || "";
-
-    // Si le cookie n'est PAS présent → login
-    if (!cookie.includes("constel_auth=ok")) {
-      return Response.redirect(
-        "https://gael-tartare.fr/constel-mentorat.html",
-        302
-      );
-    }
-  }
-
-  // Sinon on laisse passer
-  return next();
+  return context.next();
 }
